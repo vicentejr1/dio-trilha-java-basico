717b79af3ce08da593ff5f199096ac875567da9b
Vicente Junior <vicentejunior.ij@hotmail.com>
2024-07-11 15:37:25 -0300

    modificações rapidas

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class TiposVariaveis {
+
+    public static void main(String[] args){
+
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+
+
     }
+
 }
